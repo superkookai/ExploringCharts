489814b39074f0132f6aba51b33ac5282b6ef502
superkookai <weerawut@live.com>
2025-04-14 09:38:59 +0700

    Initial Commit

---
+++ ExploringCharts/ExploringChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploringChartsApp.swift
+//  ExploringCharts
+//
+//  Created by Weerawut Chaiyasomboon on 14/04/2568.
+//
+
+import SwiftUI
+
+@main
+struct ExploringChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
